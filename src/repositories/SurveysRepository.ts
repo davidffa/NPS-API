ef2430bca4c7e38ab9083c639a1e98fe5ea8912d
David <davidfilipeamorim@gmail.com>
2021-02-24 18:19:02 +0000

    Add SurveyRepository

---
+++ src/repositories/SurveysRepository.ts
@@ -0,0 +1,7 @@
+import { EntityRepository, Repository } from "typeorm";
+import Survey from "../models/Survey";
+
+@EntityRepository(Survey)
+export default class SurveysRepository extends Repository<User> {
+  
+}
