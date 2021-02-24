c4d4d26cfc805fffd2716c1e38618d3259f9f917
David <davidfilipeamorim@gmail.com>
2021-02-24 18:25:24 +0000

    Add show surveys

--- src/controllers/SurveysController.ts
+++ src/controllers/SurveysController.ts
@@ -17,4 +17,12 @@ export default class SurveysController {
 
     return res.status(201).json(survey);
   }
+
+  async show(req: Request, res: Response) {
+    const surveysRepository = getCustomRepository(SurveysRepository);
+
+    const all = await surveysRepository.find();
+
+    return res.json(all);
+  }
 }
