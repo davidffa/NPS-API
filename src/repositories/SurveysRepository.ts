2790986313bf7373b14873daccc56132970cfd6d
David <davidfilipeamorim@gmail.com>
2021-02-25 15:52:05 +0000

    Remove unused import

--- src/repositories/SurveysRepository.ts
+++ src/repositories/SurveysRepository.ts
@@ -1,6 +1,5 @@
 import { EntityRepository, Repository } from "typeorm";
 import Survey from "../models/Survey";
-import User from "../models/User";
 
 @EntityRepository(Survey)
-export default class SurveysRepository extends Repository<User> {}
+export default class SurveysRepository extends Repository<Survey> {}
